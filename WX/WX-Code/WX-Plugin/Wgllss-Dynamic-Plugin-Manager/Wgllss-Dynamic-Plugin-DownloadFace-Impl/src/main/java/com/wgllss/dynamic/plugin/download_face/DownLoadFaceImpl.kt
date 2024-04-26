00378bcfa9ace5fc0367be3299a7a5d1b0bf5b6e
wgllss <283495283@qq.com>
2024-04-26 11:12:02 +0800

    示例切换动态切换配置 文件修改

--- WX/WX-Code/WX-Plugin/Wgllss-Dynamic-Plugin-Manager/Wgllss-Dynamic-Plugin-DownloadFace-Impl/src/main/java/com/wgllss/dynamic/plugin/download_face/DownLoadFaceImpl.kt
+++ WX/WX-Code/WX-Plugin/Wgllss-Dynamic-Plugin-Manager/Wgllss-Dynamic-Plugin-DownloadFace-Impl/src/main/java/com/wgllss/dynamic/plugin/download_face/DownLoadFaceImpl.kt
@@ -1,5 +1,7 @@
 package com.wgllss.dynamic.plugin.download_face
 
+import android.text.TextUtils
+import com.wgllss.core.units.DeviceIdUtil
 import com.wgllss.dynamic.host.lib.constant.DynamicPluginConstant.CDLFD
 import com.wgllss.dynamic.host.lib.constant.DynamicPluginConstant.CLMD
 import com.wgllss.dynamic.host.lib.constant.DynamicPluginConstant.COMMON
@@ -14,11 +16,24 @@ import com.wgllss.dynamic.host.lib.constant.DynamicPluginConstant.WEB_ASSETS
 import com.wgllss.dynamic.host.lib.download.IDynamicDownLoadFace
 
 class DownLoadFaceImpl : IDynamicDownLoadFace {
+
+    private var baseXL: String = ""
+
     //    override fun getHostL() = "http://192.168.3.21:8080/assets/WXDynamicPlugin/"
 //    override fun getHostL() = "http://192.168.1.9:8080/assets/WXDynamicPlugin/"
     override fun getHostL() = "https://gitee.com/wgllss888/WXDynamicPlugin/raw/master/WX-Resource/"
 
-    override fun getBaseL() = "${getHostL()}10000_Test/"
+    /** 0:WXDynamicPlugin 动态化插件框架 理论上已经做到了可以完全不动宿主,但是如果一定要动宿主 可以提供以下思路:
+     *  1:可以根据 宿主版本号得到 宿主版本支持的 的插件,
+     *  2:当宿主必须 需要升级时,升级后原版本的插件不可用了，插件配置在新宿主版本文件夹下面，原宿主版本文件夹可可以先动态配置 在启动页 升级下载新的宿主
+     *  @example  宿主版本 10000 版本支持的插件 放在服务端 WXDynamicPlugin/10000/ 文件夹下  20000版本的插件放在 WXDynamicPlugin/20000/下面
+     */
+    override fun getBaseL(): String {
+        if (TextUtils.isEmpty(baseXL)) {
+            baseXL = StringBuilder().append(getHostL()).append(DeviceIdUtil.getDeviceId()).append("/").append("10000").append("/").toString()
+        }
+        return baseXL
+    }
 
     override fun isDebug() = true
 
